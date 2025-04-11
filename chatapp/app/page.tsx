014619ee839eaffb7c82475ed1589914435cdefc
kdingn <koukou3335@gmail.com>
2025-04-11 19:55:07 +0900

    テキストフィールドの追加

--- chatapp/app/page.tsx
+++ chatapp/app/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+import { TextField } from "@mui/material";
+
 function page() {
-  return <div>Enter</div>;
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          height: "100vh",
+        }}
+      >
+        <TextField
+          multiline
+          fullWidth
+          sx={{
+            maxWidth: "600px",
+            margin: "10px",
+            backgroundColor: "background.paper", // テーマの paper 色を適用
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default page;
