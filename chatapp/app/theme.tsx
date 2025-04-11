c00bdb92c68cbabe1c9e604f2dca120e7d2147f8
kdingn <koukou3335@gmail.com>
2025-04-11 18:03:26 +0900

    cssbaseline を追加

--- chatapp/app/theme.tsx
+++ chatapp/app/theme.tsx
@@ -1,10 +1,16 @@
 "use client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export function Theme({ children }: { children: React.ReactNode }) {
   const theme = createTheme({
     palette: {},
   });
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 }
