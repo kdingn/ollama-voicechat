0ef0bdf012b7665328c2ee5740d81909f42e7943
kdingn <koukou3335@gmail.com>
2025-04-11 18:33:49 +0900

    ayu の color を定義

--- chatapp/app/theme.tsx
+++ chatapp/app/theme.tsx
@@ -2,6 +2,7 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { light, mirage } from "ayu";
 
 export function Theme({ children }: { children: React.ReactNode }) {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -9,6 +10,27 @@ export function Theme({ children }: { children: React.ReactNode }) {
   const theme = createTheme({
     palette: {
       mode: prefersDarkMode ? "dark" : "light",
+      ...(prefersDarkMode
+        ? {
+            background: {
+              default: mirage.editor.bg.hex(),
+              paper: mirage.editor.bg.hex(),
+            },
+            text: {
+              primary: mirage.editor.fg.hex(),
+              secondary: mirage.common.accent.hex(),
+            },
+          }
+        : {
+            background: {
+              default: light.editor.bg.hex(),
+              paper: light.editor.bg.hex(),
+            },
+            text: {
+              primary: light.editor.fg.hex(),
+              secondary: light.common.accent.hex(),
+            },
+          }),
     },
   });
 
