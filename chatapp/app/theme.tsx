31f220e7a904dbb2aa83f8aa2a038a4c356d9568
kdingn <koukou3335@gmail.com>
2025-04-11 18:05:08 +0900

    システムカラーを利用するようにテーマを変更

--- chatapp/app/theme.tsx
+++ chatapp/app/theme.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export function Theme({ children }: { children: React.ReactNode }) {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
   const theme = createTheme({
-    palette: {},
+    palette: {
+      mode: prefersDarkMode ? "dark" : "light",
+    },
   });
 
   return (
