fdc49b3a76d50a7b55377a96308aa2c0eaf9a6e2
kdingn <koukou3335@gmail.com>
2025-04-12 21:30:36 +0900

    送信アイコンの追加

--- chatapp/app/components/InputForm.tsx
+++ chatapp/app/components/InputForm.tsx
@@ -1,15 +1,26 @@
-import { TextField } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import { IconButton, InputBase } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 
-export default function InputForm() {
+export default function CustomizedInputForm() {
+  const placeholder = "Input message here...";
   return (
-    <TextField
-      multiline
-      fullWidth
+    <Paper
       sx={{
+        display: "flex",
         maxWidth: "600px",
-        marginX: "10px",
-        backgroundColor: "background.paper",
+        width: "100vw",
       }}
-    />
+    >
+      <InputBase
+        multiline
+        fullWidth
+        placeholder={placeholder}
+        sx={{ fontSize: "1.1rem", padding: "6px 0px 6px 10px" }}
+      />
+      <IconButton sx={{ alignSelf: "center", margin: "0px 6px 0px 4px" }}>
+        <SendIcon />
+      </IconButton>
+    </Paper>
   );
 }
--- chatapp/app/page.tsx
+++ chatapp/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import "./styles/global.css";
+import "./global.css";
 import InputForm from "./components/InputForm";
 
 function page() {
   return (
     <div>
-      <div className="inputform">
+      <div className="chat-input-form">
         <InputForm />
       </div>
     </div>
