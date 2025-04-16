6428b780db489b11f8cfa695c1785e47d3f28646
kdingn <koukou3335@gmail.com>
2025-04-17 02:19:40 +0900

    メッセージの表示をよりチャットに近い形式に変更

--- chatapp/app/components/MessageBoard.tsx
+++ chatapp/app/components/MessageBoard.tsx
@@ -1,23 +1,34 @@
 import { useMessages } from "@/app/state";
-import { List, ListItem, ListItemText } from "@mui/material";
+import { Box, List, ListItem, ListItemText } from "@mui/material";
 
 export default function MessageBoard() {
   const { messages } = useMessages();
 
   return (
-    <List sx={{ paddingTop: 1, paddingX: 1 }}>
+    <List sx={{ padding: 1, paddingBottom: 6 }}>
       {messages.map((message, index) => (
         <ListItem
           key={index}
-          divider
           sx={{
-            backgroundColor: "background.paper",
-            borderRadius: 2,
-            marginBottom: 1,
-            textAlign: message.role === "user" ? "right" : "left",
+            justifyContent: message.role === "user" ? "flex-end" : "flex-start",
+            paddingY: 0.8,
           }}
         >
-          <ListItemText primary={message.message} />
+          <Box
+            sx={{
+              backgroundColor: "background.paper",
+              borderRadius: 2,
+              paddingX: 2,
+              paddingY: 0.5,
+              maxWidth: "80%",
+              textAlign: "left",
+            }}
+          >
+            <ListItemText
+              primary={message.message}
+              sx={{ whiteSpace: "pre-line" }}
+            />
+          </Box>
         </ListItem>
       ))}
     </List>
